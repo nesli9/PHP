23c2100fc5a40725482828dc04a9966027c92b6e
nesli9 <doanneslihan@gmail.com>
2022-10-27 13:10:16 +0300

    degisken tanımlama

---
+++ degiskenler/degisken-tanimlama.php
@@ -0,0 +1,19 @@
+<?php   
+
+    $sayi1 = 10;
+    $sayi2 = 20;
+
+    /*
+    $1sayi ==> yanlış tanımlama
+    değişkenler sayısal karakterler ile başlayamaz
+    */
+    
+    $sayi_3 = 30;
+
+    $isim = "sadık";
+    $soyad = "turan";
+
+    //echo $sayi1." ".$sayi2;
+    echo $isim." ".$soyad." ";
+
+?>
