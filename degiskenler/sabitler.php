2927837de7886e8df37c190313c5d4b21d9161cd
nesli9 <doanneslihan@gmail.com>
2022-11-01 00:24:01 +0300

    sabitler

---
+++ degiskenler/sabitler.php
@@ -0,0 +1,16 @@
+<?php
+
+    #php sabitler
+    #sabitler veri saklamak için kullanılır ancak atanan değer değiştitilemez.
+
+    define("baglanti","mssql"); //bağlantiya karşılık gelen değer mssql dir.
+    define("username","sadık"); 
+    const password = "1234";
+
+
+    echo baglanti."<br>";
+    echo username."<br>";
+    echo password."<br>";
+
+
+?>
