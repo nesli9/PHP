aab9645ff84907e4aa74d8ab8bbe86e6fc9e6879
nesli9 <doanneslihan@gmail.com>
2022-11-07 13:03:01 +0300

    if/else

---
+++ kosul-ifadeleri/if-else.php
@@ -0,0 +1,23 @@
+<?php
+
+$username = "sadikturan";
+$password = "1234";
+
+if ($username == "sadikturan") {
+    if ($password=="1234") {
+        echo "uygulamaya hoş geldiniz.";
+    } else {
+        echo "parola yanlış";
+    }
+} else {
+    echo "username yanlış.";
+}
+
+# soru
+# soru
+# soru
+# soru
+# diğer
+
+?>
+
